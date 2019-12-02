f82d306a3d06780526c7823df550fe9f77c09876
TylerM414 <tylermapson@gmail.com>
2019-12-02 09:52:03 -0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 const DB_URL = ('mongodb+srv://dbTylerM:yh5MsVCeQq9Md3Df@appointment-pljpa.mongodb.net/test?retryWrites=true&w=majority')
 
 app.set('view engine', 'ejs')
@@ -9,6 +11,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect( DB_URL, {
@@ -24,5 +27,6 @@ mongoose.connect( DB_URL, {
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
